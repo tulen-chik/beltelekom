f4ffe738208afa79256506c838cfc048e9c4f570
Tuleniu <stecniki@gmail.com>
2024-11-10 19:02:52 +0300

    added --> management images

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,21 @@
 import Image from 'next/image'
 import { Phone } from 'lucide-react'
+import Link from "next/link";
 
 export default function Header() {
     return (
         <header className="w-full bg-white border-b p-4 flex justify-between items-center">
             <div className="flex items-center gap-2">
-                <Image
-                    src="/logo-beltelekom.png"
-                    alt="Белтелеком"
-                    width={100}
-                    height={100}
-                    className="object-contain"
-                />
+                <Link href="/">
+                    <Image
+                        src="/logo-beltelekom.png"
+                        alt="Белтелеком"
+                        width={100}
+                        height={100}
+                        className="object-contain"
+                    />
+                </Link>
+
                 <div className="flex items-center text-white bg-[#1a365d] px-3 py-2 rounded">
                     <Phone className="w-5 h-5 mr-2"/>
                     <span className="font-arial text-xs">
